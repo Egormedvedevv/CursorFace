8d632f36a4aaa093f892ae4b49680ee968b83ae9
EgorMedvedev <Egormedvedev.im@gmail.com>
2026-01-01 18:02:32 +0700

    Configure for deployment to /main folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  base: '/CursorFace/',
+  base: '/CursorFace/main/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
